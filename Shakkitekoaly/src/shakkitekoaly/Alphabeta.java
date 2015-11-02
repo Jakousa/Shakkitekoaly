c3a79b26eee736cfe5b6276d584fe1fd1650971e
Jakousa <jakousa@cs.helsinki.fi>
2015-11-03 00:37:59 +0200

    aloitettiin tekoaly

---
+++ Shakkitekoaly/src/shakkitekoaly/Alphabeta.java
@@ -0,0 +1,19 @@
+package shakkitekoaly;
+
+/**
+ *
+ * @author hatchy
+ */
+public class Alphabeta {
+
+    private Lauta lauta;
+    private int syvyys;
+    private boolean pelaaja;
+    
+    public Alphabeta(Lauta lauta, boolean pelaaja, int syvyys){
+        this.lauta = lauta;
+        this.pelaaja = pelaaja;
+        this.syvyys = syvyys;
+    }
+    
+}
