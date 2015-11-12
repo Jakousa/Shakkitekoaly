e0a6abb1fb36ee155b4e43b906934b7a5f8a578e
Jakousa <jakousa@cs.helsinki.fi>
2015-11-12 23:14:33 +0200

    Testit valmiit viikko 3

--- Shakkitekoaly/src/shakkitekoaly/Shakkitekoaly.java
+++ Shakkitekoaly/src/shakkitekoaly/Shakkitekoaly.java
@@ -11,20 +11,21 @@ public class Shakkitekoaly {
     public static void main(String[] args) {
         
         //Testikoodia ja muuta keskeneräistä
-        Alphabeta t = new Alphabeta(true, 3);
-        Alphabeta v = new Alphabeta(false, 2);
-        Lauta l = new Lauta();
-
-        l.piirraLauta();
-        while (t.arvioiLauta(l.getNappulat()) < 10000
-                && t.arvioiLauta(l.getNappulat()) > -10000) {
-            t.teeSiirto(l);
-            v.teeSiirto(l);
-            System.out.println("");
-            l.piirraLauta();
-            
-            System.out.println("t: " + t.arvioiLauta(l.getNappulat()));
-            System.out.println("v: " + v.arvioiLauta(l.getNappulat()));
-        }
+//        Alphabeta t = new Alphabeta(true, 3);
+//        Alphabeta v = new Alphabeta(false, 2);
+//        Lauta l = new Lauta();
+//
+//        l.piirraLauta();
+//        while (t.arvioiLauta(l.getNappulat()) < 10000
+//                && t.arvioiLauta(l.getNappulat()) > -10000) {
+//            t.teeSiirto(l);
+//            v.teeSiirto(l);
+//            System.out.println("");
+//            l.piirraLauta();
+//            
+//            System.out.println("t: " + t.arvioiLauta(l.getNappulat()));
+//            System.out.println("v: " + v.arvioiLauta(l.getNappulat()));
+//        }
+        
     }
 }
--- Shakkitekoaly/test/shakkitekoaly/LautaTest.java
+++ Shakkitekoaly/test/shakkitekoaly/LautaTest.java
@@ -11,19 +11,116 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import shakkitekoaly.nappula.*;
 
 /**
  *
  * @author hatchy
  */
 public class LautaTest {
-    
+
     Lauta l = new Lauta();
-    
+
     @Test
-    public void tyhjaLautaAlustetaanOikein(){
-        assertTrue(l.getNappulat().length == (8*4));
+    public void tyhjaLautaAlustetaanOikein() {
+        assertTrue(l.getNappulat().length == (8 * 4));
+    }
+
+    @Test
+    public void laudanKopiointiTesti() {
+        Lauta h = new Lauta();
+
+        Nappula[] nappulat = new Nappula[2];
+        nappulat[0] = new Kuningas(new Sijainti(3, 3), true);
+        nappulat[1] = new Kuningas(new Sijainti(5, 5), false);
+
+        h = new Lauta(nappulat);
+        nappulat[0].setSijainti(new Sijainti(0, 0));
+        nappulat[1].setSijainti(new Sijainti(1, 1));
+
+        assertTrue(h.getNappulat().length == 2);
+        for (int i = 0; i < nappulat.length; i++) {
+            assertTrue(nappulat[i] != h.getNappulat()[0]);
+            assertTrue(nappulat[i] != h.getNappulat()[1]);
+        }
+
+    }
+
+    @Test
+    public void nappuloidenSiirtoTesti() {
+        Nappula n = new Kuningatar(new Sijainti(4, 4), true);
+        Sijainti uusi = new Sijainti(5, 5);
+        Nappula[] nappulat = new Nappula[1];
+        nappulat[0] = n;
+        Lauta h = new Lauta(nappulat);
+        h.siirraNappulaa(n, new Sijainti(5, 5));
+
+        assertTrue(n.getSijainti().equals(uusi));
+    }
+
+    @Test
+    public void nappuloidenSyontiTesti() {
+        Nappula n = new Kuningatar(new Sijainti(4, 4), true);
+        Nappula m = new Kuningatar(new Sijainti(4, 6), false);
+        Nappula[] nappulat = new Nappula[2];
+        nappulat[0] = n;
+        nappulat[1] = m;
+        Lauta s = new Lauta(nappulat);
+        
+        assertTrue(s.getNappulat().length == 2);
+        boolean tosi = s.siirraNappulaa(m, n.getSijainti());
+        boolean myostosi = n.getSijainti() == m.getSijainti();
+        assertTrue(tosi);
+        assertTrue(myostosi);
+        assertTrue(s.getNappulat().length == 1);
     }
     
+    @Test
+    public void omanSyontiTesti() {
+        Nappula n = new Kuningatar(new Sijainti(4, 4), true);
+        Nappula m = new Kuningatar(new Sijainti(4, 6), true);
+        Nappula[] nappulat = new Nappula[2];
+        nappulat[0] = n;
+        nappulat[1] = m;
+        Lauta s = new Lauta(nappulat);
+        
+        boolean epatosi = s.siirraNappulaa(m, n.getSijainti());
+        assertTrue(!epatosi);
+    }
+
+    @Test
+    public void sotilaidenSiirtojaSyonti() {
+        Nappula sV = new Sotilas(new Sijainti(2, 3), false);
+        Nappula s = new Sotilas(new Sijainti(4, 4), true);
+        Nappula[] nappulat = new Nappula[2];
+        nappulat[0] = s;
+        nappulat[1] = sV;
+        
+        Lauta h = new Lauta(nappulat);
+
+        
+        assertTrue(!(h.siirraNappulaa(sV, new Sijainti(2,2))));
+        assertTrue(h.siirraNappulaa(sV, new Sijainti(3,3)));
+        assertTrue(h.getNappulat().length == 2);
+        assertTrue(h.siirraNappulaa(sV, new Sijainti(4,4)));
+        assertTrue(h.getNappulat().length == 1);
+    }
     
+    @Test
+    public void reitinLoyto() {
+        Nappula n = new Kuningatar(new Sijainti(2, 2), true);
+        Nappula m = new Kuningatar(new Sijainti(3, 3), false);
+        Nappula[] nappulat = new Nappula[2];
+        nappulat[0] = n;
+        nappulat[1] = m;
+        Lauta s = new Lauta(nappulat);
+
+        boolean eitosi = s.siirraNappulaa(m, new Sijainti(1, 1));
+        boolean eimyoskaan = m.getSijainti().equals(new Sijainti(1, 1));
+        assertTrue(!eitosi);
+        assertTrue(!eimyoskaan);
+        assertTrue(m.getSijainti().equals(new Sijainti(3, 3)));
+        
+        assertTrue(s.getNappulat().length == 2);
+    }
 }
